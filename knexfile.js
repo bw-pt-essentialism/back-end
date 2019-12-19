9521da3c0b16056b6c15f64f1cfccf0ac31c055c
Joshua Draper <josh.drapergame@gmail.com>
2019-12-18 23:44:33 -0700

    Completed migrations (i believe)

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,12 @@ module.exports = {
       filename: './database/auth.db3'
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
+    },
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
