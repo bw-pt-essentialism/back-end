24c31a23301e1ef00552f9e740e98c0e06bf662c
Joshua Draper <josh.drapergame@gmail.com>
2019-12-19 20:29:51 -0700

    timer keeps going off, not too much progress

--- routes/users/users-helper.js
+++ routes/users/users-helper.js
@@ -1,1 +1,38 @@
 const db = require('../../database/dbConfig.js')
+
+function getUsers() {
+    return db('users')
+}
+
+function getUserById(id) {
+    return db('projects')
+        .where({id})
+}
+
+function addUser(body){
+    return db
+        .insert({body})
+        .into('users')
+        .then(res => {
+            const id = res[0]
+            return db('users')
+                .where({id})
+        })
+}
+
+function updUser(id, body) {
+    return db('users')
+        .where({id})
+        .update({body})
+        .then(res => {
+            const id = res[0]
+            return db('users')
+        })
+}
+
+module.exports = {
+    getUserById,
+    getUsers,
+    addUser,
+    delUser,
+}
